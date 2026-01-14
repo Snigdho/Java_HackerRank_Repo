4fb92ecce042e1f6eb4d66eed81becc6c8714d95
Snigdho <snigdhotf@gmail.com>
2026-01-14 21:30:32 +0100

    added problem 29

---
+++ 29. Java 2D Array/Solution.java
@@ -0,0 +1,55 @@
+// https://www.hackerrank.com/challenges/java-2d-array/problem
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(
+                        bufferedReader
+                            .readLine()
+                            .replaceAll("\\s+$", "")
+                            .split(" ")
+                    )
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < (arr.size() - 2); i++) {
+            for (int j = 0; j < (arr.get(0).size() - 2); j++) {
+                int hgSum =
+                    arr.get(i).get(j) +
+                    arr.get(i).get(j + 1) +
+                    arr.get(i).get(j + 2) +
+                    arr.get(i + 1).get(j + 1) +
+                    arr.get(i + 2).get(j) +
+                    arr.get(i + 2).get(j + 1) +
+                    arr.get(i + 2).get(j + 2);
+                if (hgSum > maxSum) {
+                    maxSum = hgSum;
+                }
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
