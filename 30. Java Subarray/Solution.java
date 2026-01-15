b2e4383317563aa822f8ec2a1a640de19da41d89
Snigdho <snigdhotf@gmail.com>
2026-01-15 22:37:19 +0100

    added problem 30

---
+++ 30. Java Subarray/Solution.java
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/java-negative-subarray/problem
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        scan.close();
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
