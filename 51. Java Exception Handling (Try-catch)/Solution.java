ef081e38e9fe3c0d60352fdb74186dce0562d93c
Snigdho <snigdhotf@gmail.com>
2026-02-05 19:20:15 +0100

    added problem 51

---
+++ 51. Java Exception Handling (Try-catch)/Solution.java
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            System.out.println((x / y));
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            scan.close();
+        }
+    }
+}
