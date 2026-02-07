0156be90659b750b7e1c9581d54fce6dc69ed9ba
Snigdho <snigdhotf@gmail.com>
2026-02-07 12:42:42 +0100

    added problem 53

---
+++ 53. Java Varargs - Simple Addition/Solution.java
@@ -0,0 +1,65 @@
+// https://www.hackerrank.com/challenges/simple-addition-varargs/problem
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+//Write your code here
+class Add {
+
+    public void add(int... n) {
+        int sum = 0;
+        boolean isFirst = true;
+        for (int i : n) {
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                System.out.print("+");
+            }
+            System.out.print(i);
+            sum += i;
+        }
+        System.out.print("=" + sum);
+        System.out.print("\n");
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in)
+            );
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            int n6 = Integer.parseInt(br.readLine());
+            Add ob = new Add();
+            ob.add(n1, n2);
+            ob.add(n1, n2, n3);
+            ob.add(n1, n2, n3, n4, n5);
+            ob.add(n1, n2, n3, n4, n5, n6);
+            Method[] methods = Add.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
