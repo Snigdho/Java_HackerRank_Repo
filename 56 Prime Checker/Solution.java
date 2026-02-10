bd068df20884e5fda7dc86c5a461859b52f35d58
Snigdho <snigdhotf@gmail.com>
2026-02-10 21:05:15 +0100

    added problem 56

---
+++ 56 Prime Checker/Solution.java
@@ -0,0 +1,64 @@
+// https://www.hackerrank.com/challenges/prime-checker/problem
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+class Prime {
+
+    public void checkPrime(int... arr) {
+        for (int x : arr) {
+            if (this.isPrime(x)) {
+                System.out.print(Integer.toString(x) + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in)
+            );
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
